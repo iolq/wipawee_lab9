1640682cf8d8870b89acc0f14542f922c6197b53
iolq <wipawee_watt@cmu.ac.th>
2021-01-08 18:01:55 +0700

    owarimasu!!

--- lab9_3.cpp
+++ lab9_3.cpp
@@ -1,3 +1,26 @@
-//cout << "Number of data = ";
-//cout << "Mean = ";
-//cout << "Standard deviation = ";
+#include <iostream>
+#include<fstream>
+#include <iomanip>
+#include<cmath>
+
+using namespace std;
+
+int main(){
+    int count = 0;
+    float sum = 0;
+    float sum2 = 0;
+    string textline;
+    ifstream source("score.txt");
+    while (getline(source,textline)){
+        sum += atof(textline.c_str());
+        sum2 += pow(atof(textline.c_str()),2);
+        count++;
+    }
+
+    cout << "Number of data = " << count <<"\n";
+    cout << setprecision(3);
+    cout << "Mean = " << sum/count << "\n";
+    cout << "Standard deviation = " << pow(sum2/count-(pow((sum/count),2)),0.5) ;
+    source.close();
+    return 0;
+}
